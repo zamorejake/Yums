9b2a9e3c710d1e1e8abeae395e3d948a9342c795
ColtenZimm <coltenz00@gmail.com>
2023-08-22 19:41:19 -0500

    fixed tailwind pathing and added dark mode

--- server/config/tailwind.config.js
+++ server/config/tailwind.config.js
@@ -13,3 +13,33 @@ module.exports = {
     ]
 
 }
+
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    content: [
+      ...
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    ],
+    plugins: [... require('flowbite/plugin')],
+};
+
+  import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+
+export default function MyPage() {
+  return (
+    <Flowbite>
+      ...
+      <DarkThemeToggle />
+      ...
+    </Flowbite>
+  );
+}
