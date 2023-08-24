62249253b22ef0b5dc98b06f4ad72a6ae9976bd4
VinceR66 <vrook@me.com>
2023-08-24 18:32:33 -0500

    models

--- server/models/Beverage.js
+++ server/models/Beverage.js
@@ -1,7 +1,42 @@
+/*needs to be uncommented out and code below needs to be deleted
+
+const { Schema, model } = require('mongoose');
+
+const beverageSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Float,
+    required: true,
+  },
+  in_stock: {
+    type: BOOLEAN,
+    required: true,
+  },
+  has_alcohol: {
+    type: BOOLEAN,
+    required: true,
+  },
+  image: {
+    type: STRING,
+  },
+});
+
+const Beverage = model('Beverage', beverageSchema);
+
+module.exports = Beverage;
+
+*/
+
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Beverage extends Model {}
+class Beverage extends Model { }
 
 Beverage.init(
   {
@@ -44,3 +79,5 @@ Beverage.init(
 );
 
 module.exports = Beverage;
+
+
--- server/models/index copy.js
+++ server/models/index copy.js
@@ -2,13 +2,19 @@ const Admin = require('./Admin');
 const Beverage = require('./Beverage');
 const Entree = require('./Entree');
 
+
+/* needs to be commented out
+
 Admin.hasMany(Beverage, {
-  onDelete: 'CASCADE',
+ onDelete: 'CASCADE',
 });
 
 Admin.hasMany(Entree, {
-  onDelete: 'CASCADE',
+ onDelete: 'CASCADE',
 });
+
+*/
+
 
 
 module.exports = { Admin, Beverage, Entree };
