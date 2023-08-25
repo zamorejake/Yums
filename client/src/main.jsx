a386f1ec610254b06b6902a90ef893c91a70430f
Jake <zamorejake@gmail.com>
2023-08-24 19:43:29 -0500

    working on navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
 import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter([
@@ -16,13 +14,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />
-      }, {
-        path: '/matchup',
-        element: <Matchup />
-      }, {
-        path: '/matchup/:id',
-        element: <Vote />
-      },
+      }, 
     ],
   },
 ]);
