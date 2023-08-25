3fadda8a5fcc3f6ee8f8436601084c84023827e3
VinceR66 <vrook@me.com>
2023-08-24 21:29:47 -0500

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,26 +1,106 @@
 const typeDefs = `
-  type Tech {
+  type Admin {
     _id: ID!
     name: String!
+    email: String!
+    password: String!
   }
 
-  type Matchup {
+  input RestaurantInput {
+    name: String!
+    address: String!
+    city: String!
+    state: String!
+    zipcode: String!
+    phonenumber: String!
+    description: String!
+  }
+
+  type Restaurant {
     _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+    name: String!
+    address: String!
+    city: String!
+    state: String!
+    zipcode: String!
+    phonenumber: String!
+    description: String!
+  }
+
+  input EntreeInput {
+    name: String!
+    description: String!
+    price: Float!
+    in_stock: Boolean!
+    allergy: Boolean!
+    image: String
+  }
+
+  type Entree {
+    _id: ID!
+    name: String!
+    description: String!
+    price: Float!
+    in_stock: Boolean!
+    allergy: Boolean!
+    image: String
+  }
+
+  input BeverageInput {
+    name: String!
+    description: String!
+    price: Float!
+    in_stock: Boolean!
+    has_alcohol: Boolean!
+    image: String
+  }
+
+  type Beverage {
+    _id: ID!
+    name: String!
+    description: String!
+    price: Float!
+    in_stock: Boolean!
+    has_alcohol: Boolean!
+    image: String
+  }
+
+  type Auth {
+    token: ID!
+    admin: Admin
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    admin: [Admin]
+    restaurants: [Restaurant]
+    entrees: 
+    beverages:
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+    login(email: String!, password: String!): Auth
+    createRestaurant( restaurant : RestaurantInput ): Restaurant
+    createEntree( entree: EntreeInput): Entree
+    createBeverage( beverage: BeverageInput): Beverage
+    removeRestaurant(restaurantId: ID!): Restaurant
+    removeEntree(entreeId: ID!): Entree
+    removeBeverage(BeverageId: ID!): Beverage
+    updateRestaurant(id: ID!, restaurant: RestaurantInput): Restaurant
+    updateEntree(id: ID!, entree: EntreeInput): Entree
+    updateBeverage(id: ID!, beverage: BeverageInput): Beverage
   }
 `;
 
 module.exports = typeDefs;
+
+
+
+type Query {
+  tech: [Tech]
+  matchups(_id: String): [Matchup]
+}
+
+type Mutation {
+  createMatchup(tech1: String!, tech2: String!): Matchup
+  createVote(_id: String!, techNum: Int!): Matchup
+}
