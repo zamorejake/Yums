f66c91c73c45dcbe76ec5787b0b9c3d4786b2504
Diego <diegoborjas@gmail.com>
2023-08-31 20:11:49 -0500

    added logic to login to admin page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Admin from './pages/Admin.jsx';
 import NotFound from './pages/NotFound';
 import Entree from './pages/Entree';
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -14,7 +17,15 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />
-      }, 
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/admin', 
+        element: <Admin />
+      },
       {
         path: '/entree', 
         element: <Entree />
